3211fcd0f9395981d83711a0188aa8452c050d7d
Daianes26 <daianeasilva26@gmail.com>
2020-08-05 19:58:00 -0400

    Add local storage

--- script.js
+++ script.js
@@ -32,9 +32,11 @@ function lightMode(){
 function switchPageTheme(event){
    if (event.target.checked) {
        document.documentElement.setAttribute('data-theme', 'dark');
+       localStorage.setItem('theme', 'dark');
        darkMode();
    } else {
        document.documentElement.setAttribute('data-theme', 'light');
+       localStorage.setItem('theme', 'light');
        lightMode();
    }
 }
@@ -42,3 +44,14 @@ function switchPageTheme(event){
 
 //Event Listener
 toggleSwitch.addEventListener('change', switchPageTheme);
+
+//Check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
